0b26ae72a4bdb56586f51a555ef7fe6fdc64af3e
Raphael Santos <raphaelsantos0712@gmail.com>
2022-03-28 22:43:20 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,14 @@
 
 import "./App.css";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
