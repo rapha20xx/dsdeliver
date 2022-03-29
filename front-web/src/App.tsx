ac08ddb7e7019b9728a519706de9b67a263983d0
Raphael Santos <raphaelsantos0712@gmail.com>
2022-03-28 23:14:55 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,12 +3,14 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Footer from "./Footer"
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer/>
     </>
   );
 }
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,25 +1,30 @@
 /** @format */
 import "./styles.css";
-import {ReactComponent as MainImage} from './main.svg';
+import { ReactComponent as MainImage } from "./main.svg";
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-content">
+    <>
+      <div className="home-container">
+        <div className="home-content">
           <div className="home-actions">
-              <h1 className="home-title">Faça seu pedido <br /> que entregmaos <br /> pra você!!!</h1>
-              <h3 className="home-subtitle">Escolha o seu pedido em poucos minutos <br />
+            <h1 className="home-title">
+              Faça seu pedido <br /> que entregmaos <br /> pra você!!!
+            </h1>
+            <h3 className="home-subtitle">
+              Escolha o seu pedido em poucos minutos <br />
               levaremos na sua porta
-              </h3>
-              <a href="orders" className="home-btn-order">
-                FAZER PEDIDO     
-              </a>
+            </h3>
+            <a href="orders" className="home-btn-order">
+              FAZER PEDIDO
+            </a>
           </div>
           <div className="home-image">
-            <MainImage/>
+            <MainImage />
           </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
